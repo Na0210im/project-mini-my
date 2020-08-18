563eadd14aa1ece133e6b8ce632f48a710ac88b0
Yusreenamira <69498349+Yusreenamira@users.noreply.github.com>
2020-08-18 22:53:09 +0800

    Update project.py
    line 2

--- project.py
+++ project.py
@@ -9,3 +9,13 @@ task = []
 timer = threading 
 real_timer = threading
 ok_thread = true
+
+def get_entry(event = " "):
+    text = todo.get()
+    hour = int(time.get())
+    todo.delete(0, tkinter.END)
+    time.delete(0, tkinter.END)
+    todo.focus_set()
+    add_list(text, hour)
+    if 0 < hour < 999:
+        update_list()
